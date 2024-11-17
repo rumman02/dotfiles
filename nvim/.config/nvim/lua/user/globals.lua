eb8ac1d86088d37b3802b9244712231374165c4d
Rumman <126387776+rumman02@users.noreply.github.com>
2024-11-18 00:41:49 +0600

    nvim: added user globals file

---
+++ nvim/.config/nvim/lua/user/globals.lua
@@ -0,0 +1,9 @@
+--   _____ _      ____  ____          _      
+--  / ____| |    / __ \|  _ \   /\   | |     
+-- | |  __| |   | |  | | |_) | /  \  | |     
+-- | | |_ | |   | |  | |  _ < / /\ \ | |     
+-- | |__| | |___| |__| | |_) / ____ \| |____ 
+--  \_____|______\____/|____/_/    \_\______|
+--  global variables are used for reusing variables
+--  also for, access some useful settings in a single place
+
