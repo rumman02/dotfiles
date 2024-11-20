db03bc75afa497a71fc7fbd11f83f2564a078fe2
Rumman <126387776+rumman02@users.noreply.github.com>
2024-11-20 20:07:19 +0600

    nvim: (lsp) added lsp

---
+++ nvim/.config/nvim/lua/plugins/mains/lsp.lua
@@ -0,0 +1,60 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		{
+			"williamboman/mason-lspconfig.nvim",
+			dependencies = {
+				{
+					"williamboman/mason.nvim",
+					cmd = { "Mason" },
+					event = { "BufReadPre", "BufNewFile" },
+					opts = {
+						ui = {
+							icons = {
+								package_installed = " ",
+								package_pending = "󱗽 ",
+								package_uninstalled = " ",
+							},
+							border = "rounded",
+							width = 0.7,
+							height = 0.8
+						}
+					}
+				}
+			}
+		}
+	},
+	config = function()
+		local config = require("plugins.mains.settings.config")
+		local mason_lspconfig =  require("mason-lspconfig")
+		local lsp_config = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend("force", config.nvim_capabilities, cmp_nvim_lsp.default_capabilities())
+
+		-- mason lspconfig settings
+		mason_lspconfig.setup({
+			ensure_installed = config.servers,
+			automatic_installation = true
+		})
+
+		-- lsp servers settings
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				local server_configs = config.server_settings[server_name] or {}
+				lsp_config[server_name].setup({
+					capabilities = capabilities,
+					settings = server_configs.settings,
+					on_init = server_configs.on_init,
+					autostart = server_configs.autostart,
+					cmd = server_configs.cmd,
+					on_attach = config.on_attach,
+					filetypes = server_configs.filetypes,
+					root_dir = server_configs.root_dir,
+					handlers = vim.tbl_deep_extend("force", config.default_handlers, server_configs.handlers or {})
+				})
+			end,
+		})
+	end
+}
+
