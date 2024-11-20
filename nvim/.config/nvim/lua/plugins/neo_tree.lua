299c7ec1cbe917064fd232b1251b7756c19641fb
Rumman <126387776+rumman02@users.noreply.github.com>
2024-11-20 15:28:32 +0600

    nvim: (neo_tree) added global variables in neo_tree plugin

--- nvim/.config/nvim/lua/plugins/neo_tree.lua
+++ nvim/.config/nvim/lua/plugins/neo_tree.lua
@@ -15,12 +15,12 @@ return {
 		local neo_tree = REQUIRE("neo-tree")
 		neo_tree.setup({
 			close_if_last_window = true,
-			popup_border_style = "rounded",
-			enable_git_status = true,
-			enable_diagnostics = true,
+			popup_border_style = NEOTREE_POPUP_BORDER_STYLE,
+			enable_git_status = NEOTREE_GIT_SYMBOLS_ENABLE,
+			enable_diagnostics = NEOTREE_DIAGNOSTICS_ENABLE,
 			source_selector = {
-				winbar = true,
-				statusline = false,
+				winbar = NEOTREE_SOURCE_SELECTOR_WINBAR,
+				statusline = NEOTREE_SOURCE_SELECTOR_STATUSLINE,
 				sources = {
 					{ source = "filesystem", display_name = "  Files " },
 					{ source = "buffers", display_name = " 󰓩 Buffers " },
@@ -31,66 +31,66 @@ return {
 			},
 			default_component_configs = {
 				container = {
-					enable_character_fade = true
+					enable_character_fade = NEOTREE_CHARACTER_FADE_ENABLE
 				},
 				indent = {
-					indent_size = 2,
-					padding = 0,
+					indent_size = NEOTREE_INDENT_SIZE,
+					padding = NEOTREE_INDENT_PADDING,
 					with_markers = true,
-					indent_marker = "│",
-					last_indent_marker = "└",
-					highlight = "NeoTreeIndentMarker",
+					indent_marker = NEOTREE_INDENT_SYMBOLS_LINE_ICON,
+					last_indent_marker = NEOTREE_INDENT_SYMBOLS_LAST_LINE_ICON,
+					highlight = NEOTREE_INDENT_SYMBOLS_LAST_LINE,
 					with_expanders = true,
-					expander_collapsed = "",
-					expander_expanded = "",
-					expander_highlight = "NeoTreeExpander",
+					expander_collapsed = NEOTREE_EXPANDER_SYMBOLS_COLLAPSED_ICON,
+					expander_expanded = NEOTREE_EXPANDER_SYMBOLS_EXPANDED_ICON,
+					expander_highlight = NEOTREE_EXPANDER_SYMBOLS_HIGHLIGHTS
 				},
 				icon = {
-					folder_closed = " 󰉋 ",
-					folder_open = " 󰝰 ",
-					folder_empty = " 󰉖 ",
+					folder_closed = NEOTREE_FOLDER_SYMBOLS_CLOSED_ICON,
+					folder_open = NEOTREE_FOLDER_SYMBOLS_OPEN_ICON,
+					folder_empty = NEOTREE_FOLDER_SYMBOLS_EMPTY_ICON,
 					default = "*",
-					highlight = "NeoTreeFileIcon"
+					highlight = NEOTREE_FOLDER_SYMBOLS_HIGHLIGHTS
 				},
 				modified = {
-					symbol = " ",
-					highlight = "NeoTreeModified",
+					symbol = NEOTREE_MODIFIED_SYMBOL_ICON,
+					highlight = NEOTREE_MODIFIED_SYMBOL_HIGHLIGHTS
 				},
 				diagnostics = {
 					symbols = {
-						hint =  " ",
-						info =  " ",
-						warn =  " ",
-						error = " "
+						hint = NEOTREE_DIAGNOSTICS_SYMBOLS_HINT_ICON,
+						info = NEOTREE_DIAGNOSTICS_SYMBOLS_INFO_ICON,
+						warn = NEOTREE_DIAGNOSTICS_SYMBOLS_WARN_ICON,
+						error = NEOTREE_DIAGNOSTICS_SYMBOLS_ERROR_ICON
 					},
 					highlights = {
-						hint = "DiagnosticSignHint",
-						info = "DiagnosticSignInfo",
-						warn = "DiagnosticSignWarn",
-						error = "DiagnosticSignError"
+						hint = NEOTREE_DIAGNOSTICS_SYMBOLS_HINT_HIGHLIGHTS,
+						info = NEOTREE_DIAGNOSTICS_SYMBOLS_INFO_HIGHLIGHTS,
+						warn = NEOTREE_DIAGNOSTICS_SYMBOLS_WARN_HIGHLIGHTS,
+						error = NEOTREE_DIAGNOSTICS_SYMBOLS_ERROR_HIGHLIGHTS
 					},
 				},
 				name = {
-					use_git_status_colors = true,
-					highlight = "NeoTreeFileName",
+					use_git_status_colors = NEOTREE_GIT_SYMBOLS_COLORS_ENABLE,
+					highlight = NEOTREE_GIT_SYMBOLS_HIGHLIGHTS
 				},
 				git_status = {
 					symbols = {
-						added = " ",
-						modified = "󱗝 ",
-						deleted = "󱋯 ",
-						renamed = " ",
-						untracked = " ",
-						ignored = " ",
-						unstaged = " ",
-						staged = " ",
-						conflict = " ",
+						added = NEOTREE_GIT_SYMBOLS_ADDED_ICON,
+						modified = NEOTREE_GIT_SYMBOLS_MODIFIED_ICON,
+						deleted = NEOTREE_GIT_SYMBOLS_DELETED_ICON,
+						renamed = NEOTREE_GIT_SYMBOLS_RENAMED_ICON,
+						untracked = NEOTREE_GIT_SYMBOLS_UNTRACKED_ICON,
+						ignored = NEOTREE_GIT_SYMBOLS_IGNORED_ICON,
+						unstaged = NEOTREE_GIT_SYMBOLS_UNSTAGED_ICON,
+						staged = NEOTREE_GIT_SYMBOLS_STAGED_ICON,
+						conflict = NEOTREE_GIT_SYMBOLS_CONFLICT_ICON
 					}
 				},
 			},
 			window = {
-				position = "left",
-				width = 40,
+				position = NEOTREE_DEFAULT_POSITION,
+				width = NEOTREE_DEFAULT_WINDOW_SIZE,
 				mapping_options = {
 					noremap = true,
 					nowait = true,
@@ -146,57 +146,50 @@ return {
 					["?"] = "none",
 
 					-- remapping 
-					[ "\\fpo"] = {"toggle_preview", desc = "Open", config = {use_float = true, use_image_nvim = true}},
-					[ "\\fpf"] = {"focus_preview", desc = "Focus"},
+					[ BUFLEADER .. "fpo"] = {"toggle_preview", desc = "Open", config = {use_float = true, use_image_nvim = true}},
+					[ BUFLEADER .. "fpf"] = {"focus_preview", desc = "Focus"},
 
-					[ "\\foh"] = {"open_leftabove_vs", desc = "Left"},
-					[ "\\fol"] = {"open_vsplit", desc = "Right"},
-					[ "\\foj"] = {"open_split", desc = "Down"},
+					[ BUFLEADER .. "foh"] = {"open_leftabove_vs", desc = "Left"},
+					[ BUFLEADER .. "fol"] = {"open_vsplit", desc = "Right"},
+					[ BUFLEADER .. "foj"] = {"open_split", desc = "Down"},
 
-					[ "\\fopc"] = {"open_with_window_picker", desc = "Current"},
-					[ "\\foph"] = {"split_with_window_picker", desc = "Horizontal"},
-					[ "\\fopv"] = {"vsplit_with_window_picker", desc = "Vertical"},
+					[ BUFLEADER .. "fopc"] = {"open_with_window_picker", desc = "Current"},
+					[ BUFLEADER .. "foph"] = {"split_with_window_picker", desc = "Horizontal"},
+					[ BUFLEADER .. "fopv"] = {"vsplit_with_window_picker", desc = "Vertical"},
 
-					[ "\\fotn"] = {"open_tabnew", desc = "New"},
-					[ "\\fotd"] = {"open_tab_drop", desc = "Drop"},
+					[ BUFLEADER .. "fotn"] = {"open_tabnew", desc = "New"},
+					[ BUFLEADER .. "fotd"] = {"open_tab_drop", desc = "Drop"},
 
-					[ "\\fd"] = {"delete", desc = "Delete" },
-					[ "\\fr"] = {"rename", desc = "Rename" },
-					[ "\\fa"] = { "add", desc = "Add" },
-					[ "\\da"] = {"add_directory", desc = "Add" },
+					[ BUFLEADER .. "fd"] = {"delete", desc = "Delete" },
+					[ BUFLEADER .. "fr"] = {"rename", desc = "Rename" },
+					[ BUFLEADER .. "fa"] = { "add", desc = "Add" },
+					[ BUFLEADER .. "da"] = {"add_directory", desc = "Add" },
 
-					[ "\\cc"] = {"close_node", desc = "Current node"},
-					[ "\\cs"] = {"close_all_subnodes", desc = "Sub & current nodes"},
-					[ "\\ca"] = {"close_all_nodes", desc = "All nodes"},
-					[ "\\ea"] = {"expand_all_nodes", desc = "All nodes"},
+					[ BUFLEADER .. "cc"] = {"close_node", desc = "Current node"},
+					[ BUFLEADER .. "cs"] = {"close_all_subnodes", desc = "Sub & current nodes"},
+					[ BUFLEADER .. "ca"] = {"close_all_nodes", desc = "All nodes"},
+					[ BUFLEADER .. "ea"] = {"expand_all_nodes", desc = "All nodes"},
 
-					[ "\\i"] = {"show_file_details", desc = "Show details" },
-					[ "\\r"] = {"refresh", desc = "Refresh" },
-					[ "\\p"] = {"paste_from_clipboard", desc = "Paste" },
-					[ "\\<esc>"] = {"close_window",desc = "Quit" },
-					[ "\\x"] = {"cut_to_clipboard", desc = "Cut" },
-					[ "\\y"] = {"copy_to_clipboard", desc = "Copy" },
+					[ BUFLEADER .. "i"] = {"show_file_details", desc = "Show details" },
+					[ BUFLEADER .. "r"] = {"refresh", desc = "Refresh" },
+					[ BUFLEADER .. "p"] = {"paste_from_clipboard", desc = "Paste" },
+					[ BUFLEADER .. "<esc>"] = {"close_window",desc = "Quit" },
+					[ BUFLEADER .. "x"] = {"cut_to_clipboard", desc = "Cut" },
+					[ BUFLEADER .. "y"] = {"copy_to_clipboard", desc = "Copy" },
 				}
 			},
 			filesystem = {
 				filtered_items = {
-					visible = false,
-					hide_dotfiles = false,
-					hide_gitignored = true,
-					hide_hidden = false,
-					hide_by_name = {
-						".git",
-						"node_modules"
-					},
-					hide_by_pattern = {},
-					always_show = {},
-					always_show_by_pattern = {},
-					never_show = {},
-					never_show_by_pattern = {},
-				},
-				follow_current_file = {
-					enabled = false,
-					leave_dirs_open = false,
+					visible = NEOTREE_FILTERED_ITEMS_VISIBLE,
+					hide_dotfiles = NEOTREE_FILTERED_ITEMS_HIDE_DOTFILES,
+					hide_gitignored = NEOTREE_FILTERED_ITEMS_HIDE_GITIGNORED,
+					hide_hidden = NEOTREE_FILTERED_ITEMS_HIDE_HIDDEN,
+					hide_by_name = NEOTREE_FILTERED_ITEMS_HIDE_BY_NAME,
+					hide_by_pattern = NEOTREE_FILTERED_ITEMS_HIDE_BY_PATTERN,
+					always_show = NEOTREE_FILTERED_ITEMS_ALWAYS_SHOW,
+					always_show_by_pattern = NEOTREE_FILTERED_ITEMS_ALWAYS_SHOW_BY_PATTERNS,
+					never_show = NEOTREE_FILTERED_ITEMS_NEVER_SHOW,
+					never_show_by_pattern = NEOTREE_FILTERED_ITEMS_NEVER_SHOW_BY_PATTERNS
 				},
 				group_empty_dirs = false,
 				hijack_netrw_behavior = "open_default",
@@ -209,20 +202,20 @@ return {
 						["s"] = "fuzzy_finder",
 
 						-- remapping 
-						[ "\\<"] = {"navigate_up", desc = "Go back"},
-						[ "\\>"] = {"set_root", desc = "Go front"},
-						[ "\\h"] = {"toggle_hidden", desc = "Show hidden"},
-						[ "\\ff"] = {"fuzzy_finder", desc = "Find" }, -- fuzzy sorting using the fzy algorithm
-						[ "\\df"] = {"fuzzy_finder_directory", desc = "Find"},
-						[ "\\gmp"] = {"prev_git_modified", desc = "Previous" },
-						[ "\\gmn"] = {"next_git_modified", desc = "Next" },
-						[ "\\oc"] = { "order_by_created", desc = "Created" },
-						[ "\\od"] = { "order_by_diagnostics", desc = "Diagnostics" },
-						[ "\\og"] = { "order_by_git_status", desc = "Git status" },
-						[ "\\om"] = { "order_by_modified", desc = "Modified" },
-						[ "\\on"] = { "order_by_name", desc = "Name" },
-						[ "\\os"] = { "order_by_size", desc = "Size" },
-						[ "\\ot"] = { "order_by_type", desc = "Type" },
+						[ BUFLEADER .. "<"] = {"navigate_up", desc = "Go back"},
+						[ BUFLEADER .. ">"] = {"set_root", desc = "Go front"},
+						[ BUFLEADER .. "h"] = {"toggle_hidden", desc = "Show hidden"},
+						[ BUFLEADER .. "ff"] = {"fuzzy_finder", desc = "Find" }, -- fuzzy sorting using the fzy algorithm
+						[ BUFLEADER .. "df"] = {"fuzzy_finder_directory", desc = "Find"},
+						[ BUFLEADER .. "gmp"] = {"prev_git_modified", desc = "Previous" },
+						[ BUFLEADER .. "gmn"] = {"next_git_modified", desc = "Next" },
+						[ BUFLEADER .. "oc"] = { "order_by_created", desc = "Created" },
+						[ BUFLEADER .. "od"] = { "order_by_diagnostics", desc = "Diagnostics" },
+						[ BUFLEADER .. "og"] = { "order_by_git_status", desc = "Git status" },
+						[ BUFLEADER .. "om"] = { "order_by_modified", desc = "Modified" },
+						[ BUFLEADER .. "on"] = { "order_by_name", desc = "Name" },
+						[ BUFLEADER .. "os"] = { "order_by_size", desc = "Size" },
+						[ BUFLEADER .. "ot"] = { "order_by_type", desc = "Type" },
 					},
 					fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
 						["<c-n>"] = "move_cursor_down",
@@ -248,38 +241,38 @@ return {
 						["d"] = "buffer_delete",
 
 						-- remapping 
-						[ "\\<"] = { "navigate_up", desc = "Go back"},
-						[ "\\>"] = { "set_root", desc = "Go front"},
-						[ "\\bd"] = { "buffer_delete", desc = "Delete"},
-						[ "\\bo"] = { function() end, desc = "Order", nowait = false },
-						[ "\\boc"] = { "order_by_created", desc = "Created" },
-						[ "\\bod"] = { "order_by_diagnostics", desc = "Diagnostics" },
-						[ "\\bog"] = { "order_by_git_status", desc = "Git status" },
-						[ "\\bom"] = { "order_by_modified", desc = "Modified" },
-						[ "\\bon"] = { "order_by_name", desc = "Name" },
-						[ "\\bos"] = { "order_by_size", desc = "Size" },
-						[ "\\bot"] = { "order_by_type", desc = "Type" },
+						[ BUFLEADER .. "<"] = { "navigate_up", desc = "Go back"},
+						[ BUFLEADER .. ">"] = { "set_root", desc = "Go front"},
+						[ BUFLEADER .. "bd"] = { "buffer_delete", desc = "Delete"},
+						[ BUFLEADER .. "bo"] = { function() end, desc = "Order", nowait = false },
+						[ BUFLEADER .. "boc"] = { "order_by_created", desc = "Created" },
+						[ BUFLEADER .. "bod"] = { "order_by_diagnostics", desc = "Diagnostics" },
+						[ BUFLEADER .. "bog"] = { "order_by_git_status", desc = "Git status" },
+						[ BUFLEADER .. "bom"] = { "order_by_modified", desc = "Modified" },
+						[ BUFLEADER .. "bon"] = { "order_by_name", desc = "Name" },
+						[ BUFLEADER .. "bos"] = { "order_by_size", desc = "Size" },
+						[ BUFLEADER .. "bot"] = { "order_by_type", desc = "Type" },
 					}
-				},
+				}
 			},
 			git_status = {
 				window = {
 					mappings = {
-						[ "\\ga"] = { function() end, desc = "Add", nowait = false },
-						[ "\\gaa"] = { "git_add_all", desc = "All" },
-						[ "\\gac"] = { "git_add_file", desc = "Current" },
-						[ "\\gu"] = { "git_unstage_file", desc = "Unstage" },
-						[ "\\gr"] = { "git_revert_file", desc = "Revert" },
-						[ "\\gc"] = { "git_commit", desc = "Commit" },
-						[ "\\gp"] = { "git_push", desc = "Push" },
-						[ "\\gg"] = { "git_commit_and_push", desc = "Commit & push" },
-						[ "\\goc"] = { "order_by_created", desc = "Created" },
-						[ "\\god"] = { "order_by_diagnostics", desc = "Diagnostics" },
-						[ "\\gog"] = { "order_by_git_status", desc = "Git status" },
-						[ "\\gom"] = { "order_by_modified", desc = "Modified" },
-						[ "\\gon"] = { "order_by_name", desc = "Name" },
-						[ "\\gos"] = { "order_by_size", desc = "Size" },
-						[ "\\got"] = { "order_by_type", desc = "Type" },
+						[ BUFLEADER .. "ga"] = { function() end, desc = "Add", nowait = false },
+						[ BUFLEADER .. "gaa"] = { "git_add_all", desc = "All" },
+						[ BUFLEADER .. "gac"] = { "git_add_file", desc = "Current" },
+						[ BUFLEADER .. "gu"] = { "git_unstage_file", desc = "Unstage" },
+						[ BUFLEADER .. "gr"] = { "git_revert_file", desc = "Revert" },
+						[ BUFLEADER .. "gc"] = { "git_commit", desc = "Commit" },
+						[ BUFLEADER .. "gp"] = { "git_push", desc = "Push" },
+						[ BUFLEADER .. "gg"] = { "git_commit_and_push", desc = "Commit & push" },
+						[ BUFLEADER .. "goc"] = { "order_by_created", desc = "Created" },
+						[ BUFLEADER .. "god"] = { "order_by_diagnostics", desc = "Diagnostics" },
+						[ BUFLEADER .. "gog"] = { "order_by_git_status", desc = "Git status" },
+						[ BUFLEADER .. "gom"] = { "order_by_modified", desc = "Modified" },
+						[ BUFLEADER .. "gon"] = { "order_by_name", desc = "Name" },
+						[ BUFLEADER .. "gos"] = { "order_by_size", desc = "Size" },
+						[ BUFLEADER .. "got"] = { "order_by_type", desc = "Type" },
 					}
 				}
 			}
--- nvim/.config/nvim/lua/user/globals.lua
+++ nvim/.config/nvim/lua/user/globals.lua
@@ -9,6 +9,27 @@
 
 -- variables ======================================================================
 local g = vim.g
+local modified_symbol_icon = " "
+local modified_symbol_highlights = "NeoTreeModified"
+local diagnostics_symbols_hint_icon = " "
+local diagnostics_symbols_info_icon = " "
+local diagnostics_symbols_warn_icon = " "
+local diagnostics_symbols_error_icon =" "
+local diagnostics_symbols_hint_highlights = "DiagnosticSignHint"
+local diagnostics_symbols_info_highlights = "DiagnosticSignInfo"
+local diagnostics_symbols_warn_highlights = "DiagnosticSignWarn"
+local diagnostics_symbols_error_highlights = "DiagnosticSignError"
+local git_symbols_added_icon = " "
+local git_symbols_modified_icon = "󱗝 "
+local git_symbols_deleted_icon = "󱋯 "
+local git_symbols_renamed_icon = " "
+local git_symbols_untracked_icon = " "
+local git_symbols_ignored_icon = " "
+local git_symbols_unstaged_icon = " "
+local git_symbols_staged_icon = " "
+local git_symbols_conflict_icon = " "
+local border_chars_1 = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+local border_chars_2 = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
 
 -- safe require ===================================================================
 REQUIRE = function(module)
@@ -31,4 +52,67 @@ LSPLEADER = ","
 
 -- lazy plugin manager ============================================================
 LAZY = {}
+
+-- neo-tree =======================================================================
+NEOTREE_DEFAULT_POSITION = "left"
+NEOTREE_DEFAULT_WINDOW_SIZE = 40
+NEOTREE_POPUP_BORDER_STYLE = border_chars_2
+NEOTREE_CHARACTER_FADE_ENABLE = true
+
+NEOTREE_INDENT_SIZE = 2
+NEOTREE_INDENT_PADDING = 0
+NEOTREE_INDENT_SYMBOLS_LINE_ICON = "│"
+NEOTREE_INDENT_SYMBOLS_LAST_LINE_ICON = "└"
+NEOTREE_INDENT_SYMBOLS_HIGHLIGHTS = "NeoTreeIndentMarker"
+NEOTREE_EXPANDER_SYMBOLS_COLLAPSED_ICON = ""
+NEOTREE_EXPANDER_SYMBOLS_EXPANDED_ICON = ""
+NEOTREE_EXPANDER_SYMBOLS_HIGHLIGHTS = "NeoTreeExpander"
+
+NEOTREE_SOURCE_SELECTOR_WINBAR = false
+NEOTREE_SOURCE_SELECTOR_STATUSLINE = true
+
+NEOTREE_FOLDER_SYMBOLS_CLOSED_ICON = " 󰉋 "
+NEOTREE_FOLDER_SYMBOLS_OPEN_ICON = " 󰝰 "
+NEOTREE_FOLDER_SYMBOLS_EMPTY_ICON = " 󰉖 "
+NEOTREE_FOLDER_SYMBOLS_HIGHLIGHTS = "NeoTreeFileIcon"
+
+NEOTREE_MODIFIED_SYMBOL_ICON = modified_symbol_icon
+NEOTREE_MODIFIED_SYMBOL_HIGHLIGHTS = modified_symbol_highlights
+
+NEOTREE_DIAGNOSTICS_ENABLE = true
+NEOTREE_DIAGNOSTICS_SYMBOLS_HINT_ICON = diagnostics_symbols_hint_icon
+NEOTREE_DIAGNOSTICS_SYMBOLS_INFO_ICON = diagnostics_symbols_info_icon
+NEOTREE_DIAGNOSTICS_SYMBOLS_WARN_ICON = diagnostics_symbols_warn_icon
+NEOTREE_DIAGNOSTICS_SYMBOLS_ERROR_ICON = diagnostics_symbols_error_icon
+NEOTREE_DIAGNOSTICS_SYMBOLS_HINT_HIGHLIGHTS = diagnostics_symbols_hint_highlights
+NEOTREE_DIAGNOSTICS_SYMBOLS_INFO_HIGHLIGHTS = diagnostics_symbols_info_highlights
+NEOTREE_DIAGNOSTICS_SYMBOLS_WARN_HIGHLIGHTS = diagnostics_symbols_warn_highlights
+NEOTREE_DIAGNOSTICS_SYMBOLS_ERROR_HIGHLIGHTS = diagnostics_symbols_error_highlights
+
+NEOTREE_GIT_SYMBOLS_ENABLE = true
+NEOTREE_GIT_SYMBOLS_COLORS_ENABLE = true
+NEOTREE_GIT_SYMBOLS_ADDED_ICON = git_symbols_added_icon
+NEOTREE_GIT_SYMBOLS_MODIFIED_ICON = git_symbols_modified_icon
+NEOTREE_GIT_SYMBOLS_DELETED_ICON = git_symbols_deleted_icon
+NEOTREE_GIT_SYMBOLS_RENAMED_ICON = git_symbols_renamed_icon
+NEOTREE_GIT_SYMBOLS_UNTRACKED_ICON = git_symbols_untracked_icon
+NEOTREE_GIT_SYMBOLS_IGNORED_ICON = git_symbols_ignored_icon
+NEOTREE_GIT_SYMBOLS_UNSTAGED_ICON = git_symbols_unstaged_icon
+NEOTREE_GIT_SYMBOLS_STAGED_ICON = git_symbols_staged_icon
+NEOTREE_GIT_SYMBOLS_CONFLICT_ICON = git_symbols_conflict_icon
+NEOTREE_GIT_SYMBOLS_HIGHLIGHTS = "NeoTreeFileName"
+
+NEOTREE_FILTERED_ITEMS_VISIBLE = false
+NEOTREE_FILTERED_ITEMS_HIDE_DOTFILES = false
+NEOTREE_FILTERED_ITEMS_HIDE_GITIGNORED = true
+NEOTREE_FILTERED_ITEMS_HIDE_HIDDEN = false
+NEOTREE_FILTERED_ITEMS_HIDE_BY_NAME = {
+	".git",
+	"node_modules"
+}
+NEOTREE_FILTERED_ITEMS_HIDE_BY_PATTERN = {}
+NEOTREE_FILTERED_ITEMS_ALWAYS_SHOW = {}
+NEOTREE_FILTERED_ITEMS_ALWAYS_SHOW_BY_PATTERNS = {}
+NEOTREE_FILTERED_ITEMS_NEVER_SHOW = {}
+NEOTREE_FILTERED_ITEMS_NEVER_SHOW_BY_PATTERNS = {}
 
