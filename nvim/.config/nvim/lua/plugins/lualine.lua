6aff05e9996ea4eee7330580aaaeae6a0311e25f
Rumman <126387776+rumman02@users.noreply.github.com>
2024-11-21 23:48:00 +0600

    nvim: (lualine) fixed comp_sep left char

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
 		options = {
 			theme = "gruvbox",
 			globalstatus = true,
-			component_separators = { left = "", right = "╱" },
+			component_separators = { left = "╲", right = "╱" },
 			section_separators = { left = "", right = "" },
 			disabled_filetypes = {
 				statusline = {"neo-tree"},
