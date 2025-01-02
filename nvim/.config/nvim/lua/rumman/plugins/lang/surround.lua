f3a9ebadbe8cdd7ba32b57b188dcdd7e60b155e8
Rumman <126387776+rumman02@users.noreply.github.com>
2025-01-02 10:09:34 +0600

    nvim: modified -> surround keys

--- nvim/.config/nvim/lua/rumman/plugins/lang/surround.lua
+++ nvim/.config/nvim/lua/rumman/plugins/lang/surround.lua
@@ -1,4 +1,18 @@
 local surround = require("nvim-surround")
 
-surround.setup({})
+surround.setup({
+	keymaps = {
+		insert = "<C-g>s",
+		insert_line = "<C-g>S",
+		normal = "ys",
+		normal_cur = "yss",
+		normal_line = "yS",
+		normal_cur_line = "ySS",
+		visual = "S",
+		visual_line = "gS",
+		delete = "ds",
+		change = "cs",
+		change_line = "cS",
+	},
+})
 
