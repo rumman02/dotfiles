6ffde16e6bb9d996b683f440297f268b013e982b
Rumman <126387776+rumman02@users.noreply.github.com>
2025-01-02 10:06:02 +0600

    nvim: added -> dynmic terminal
    - if inside tmux, it will open tmux pane
    - in outside tmux, it will open toggleterm

--- nvim/.config/nvim/lua/rumman/lib/func.lua
+++ nvim/.config/nvim/lua/rumman/lib/func.lua
@@ -91,5 +91,112 @@ return {
 			vim.api.nvim_feedkeys("q", "n", true)
 		end
 	end,
+
+
+	-- open terminal by toggleterm or tmux pane
+	toggleterm_tmux = function (key, count)
+		---@diagnostic disable-next-line: unused-local
+		local result = vim.fn.system("tmux info")
+		local split_size, toggleterm_direction, tmux_option, tmux_reverse_direction, tmux_reverse_split_size, char
+
+		-- get expected width & height size
+		local split_ratio = 3 -- less is bigger window, higher is smaller window
+		local w_split_size = math.floor(vim.api.nvim_win_get_width(0) / split_ratio)
+		local h_split_size = math.floor(vim.api.nvim_win_get_height(0) / split_ratio)
+
+		-- ask for input of count
+		if count == 1 then
+			char = vim.fn.input("Count: ") or 1
+		end
+
+		if key == "h" or key == "l" then
+			split_size = w_split_size
+			toggleterm_direction = "vertical"
+
+			tmux_reverse_direction = "-v -l "
+			tmux_reverse_split_size = math.floor(vim.api.nvim_win_get_height(0))
+
+			if key == "h" then
+				tmux_option = "-d -h -b -l "
+			else
+				tmux_option = "-d -h -l "
+			end
+
+		elseif key == "j" or key == "k" then
+			split_size = h_split_size
+			toggleterm_direction = "horizontal"
+
+			tmux_reverse_direction = "-h -l "
+			tmux_reverse_split_size = math.floor(vim.api.nvim_win_get_width(0))
+
+			if key == "k" then
+				tmux_option = "-d -v -b -l "
+			else
+				tmux_option = "-d -v -l "
+			end
+		end
+
+		if vim.v.shell_error == 0 then
+			vim.cmd("silent! !tmux split-window " .. tmux_option .. split_size)
+			if count == 1 then
+				local split_size_per_pane = math.floor(tmux_reverse_split_size / char)
+				local nsplit_size = tmux_reverse_split_size - split_size_per_pane
+				---@diagnostic disable-next-line: unused-local
+				for i = 1, char-1, 1 do
+					vim.cmd("silent! !tmux split-window " .. tmux_reverse_direction .. nsplit_size)
+					nsplit_size = nsplit_size - split_size_per_pane
+				end
+			end
+		else
+			vim.cmd("ToggleTerm size=".. split_size .. " direction=" .. toggleterm_direction)
+			vim.cmd("wincmd " .. string.upper(key))
+			vim.cmd(toggleterm_direction .. " resize " .. split_size)
+			if count == 1 then
+				local split_size_per_pane = math.floor(tmux_reverse_split_size / char)
+				local nsplit_size = tmux_reverse_split_size - split_size_per_pane
+				for i = 1, char-1, 1 do
+					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(tostring(i+1) .. "<cmd>exe v:count1 . 'ToggleTerm direction=" .. toggleterm_direction .. "'<cr><c-w>=", true, false, true), "n", false)
+					nsplit_size = nsplit_size - split_size_per_pane
+				end
+			end
+		end
+
+		if vim.fn.exists(":NoiceDismiss") == 2 then
+			vim.cmd("NoiceDismiss")
+		end
+	end,
+
+	toggle_toggleterm_tmux = function ()
+		local result = vim.fn.system("tmux info")
+		if vim.v.shell_error == 0 then
+			vim.cmd("silent! !tmux resize-pane -Z")
+		else
+			vim.cmd("ToggleTerm")
+		end
+	end,
+
+
+	toggleterm_tmux_close = function (key)
+		local result = vim.fn.system("tmux info")
+		local tmux_direction
+		if key == "h" then
+			tmux_direction = "-L"
+		elseif key == "j" then
+			tmux_direction = "-D"
+		elseif key == "k" then
+			tmux_direction = "-U"
+		elseif key == "l" then
+			tmux_direction = "-R"
+		end
+
+		if vim.v.shell_error == 0 then
+			vim.cmd("silent! !tmux select-pane " .. tmux_direction )
+			vim.cmd("silent! !tmux kill-pane")
+		end
+
+		if vim.fn.exists(":NoiceDismiss") == 2 then
+			vim.cmd("NoiceDismiss")
+		end
+	end,
 }
 
