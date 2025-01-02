41c1e9a08e74f49f8210b19301488ac496517aa1
Rumman <126387776+rumman02@users.noreply.github.com>
2025-01-02 11:10:27 +0600

    nvim: modified -> (snacks) config file

--- nvim/.config/nvim/lua/rumman/plugins/tools/snacks.lua
+++ nvim/.config/nvim/lua/rumman/plugins/tools/snacks.lua
@@ -1,18 +1,35 @@
 local snacks = require("snacks")
+local icons = require("rumman.lib.icons")
 
 snacks.setup({
-	indent = { enabled = true },
-	input =  { enabled = true },
-	notifier = { enabled = true },
-	rename = { enabled = true },
-	scroll = {
+	indent = {
 		enabled = true,
-		animate = {
-			duration = { step = 15, total = 150 },
-			easing = "linear",
-		}
+		char = icons.ui.BorderLeft,
+		chunk = {
+			char = {
+				corner_top = icons.ui.BorderTopLeft,
+				corner_bottom = icons.ui.BorderBottomLeft,
+				horizontal = icons.ui.BorderTop,
+				vertical = icons.ui.BorderLeft,
+				arrow = icons.ui.Arrow,
+			},
+		},
 	},
-	statuscolumn = { enabled = true },
+	input = { enabled = false },
+	notifier = {
+		enabled = true,
+		timeout = 2000,
+		icons = {
+			error = icons.diagnostics.Error,
+			warn = icons.diagnostics.Warning,
+			info = icons.diagnostics.Information,
+			debug = icons.ui.Debug,
+			trace = icons.ui.Trace,
+		},
+	},
+	rename = { enabled = true },
+	scroll = { enabled = true },
+	statuscolumn = { enabled = false },
 	win = { enabled = true },
 	words = { enabled = true },
 	zen = { enabled = true },
